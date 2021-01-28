3e6c017f07784f53e26dcab7737860ba210b6206
Jonathan Rivera <johny24rivera@outlook.com>
2021-01-28 14:52:27 -0700

    mongodb changes

--- util/database.js
+++ util/database.js
@@ -1,6 +1,23 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const corsOptions = {
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://GoldCode:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+
 let _db;
 
 const mongoConnect = (callback) => {
