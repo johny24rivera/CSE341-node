0122a2f2461f535f6bd36631b0f3750e0ce91a9b
unknown <jonathan24rivera@gmail.com>
2021-02-05 16:05:17 -0800

    url parameter changed

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('6017a04610404d391cc01ba8')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+    User.findById('6017a04610404d391cc01ba8')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -38,38 +38,38 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 const corsOptions = {
-  origin: "https://cse41-webapp.herokuapp.com/",
-  optionsSuccessStatus: 200
+    origin: "https://cse41-webapp.herokuapp.com/",
+    optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://GoldCode:aTZGS2Gu0VQJU27G@cluster0.qz6md.mongodb.net/shop?retryWrites=true&w=majority";
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://GoldCode:aTZGS2Gu0VQJU27G@cluster0.qz6md.mongodb.net/shop?w=majority&retryWrites=true";
 
 mongoose
-  .connect(
-    MONGODB_URL, options
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: "Jonathan",
-          email: 'johny24rivera@gmail.com',
-          cart: { items: [] }
-        });
-        user.save();
-      }
+    .connect(
+        MONGODB_URL, options
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: "Jonathan",
+                    email: 'johny24rivera@gmail.com',
+                    cart: { items: [] }
+                });
+                user.save();
+            }
+        })
+        app.listen(PORT);
     })
-    app.listen(PORT);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .catch(err => {
+        console.log(err);
+    });
